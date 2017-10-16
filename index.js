e89e2a00691a4bf3b702cbc87a330c39e3ff4c0c
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-10-16 15:52:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import werwertq from 'test'
-  class RandomTest extends Component {
-  static PropTypes = {
-    h1: PropTypes.string,
-    h2: PropTypes.string,
-  }
-  render(){
-    let {
-      //h1 = dep(),
-      h2 = 'sup this a github edit',
-    } = this.props;
 
-    let {
-      flux, config
-    } = this.context;
+ const RandomTest = ({h2="Hello from heba, testing"}) => (<div> <h2>{h2}</h2></div>)
 
-    return (
-      <div>
-        <h2>{h2}</h2>
-      </div>
-    );
-  }
-}
 
 export default RandomTest;
